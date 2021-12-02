736f05c61359afa041c56adc9eef4f88ea37d6ba
jonghyeon <jh.won@salin.co.kr>
2021-12-02 15:52:36 +0900

    add

---
+++ src/models/filterData.ts
@@ -0,0 +1,19 @@
+export const filterData = {
+    year: [
+        { title: '전체', index: 1, query: '' },
+        { title: '고3,N수생', index: 2, query: '고3' },
+        { title: '고2', index: 3, query: '고2' },
+        { title: '고1', index: 4, query: '고1' },
+        { title: '중3', index: 5, query: '중3' },
+        { title: '중2', index: 6, query: '중2' },
+        { title: '중1', index: 7, query: '중1' },
+    ],
+    subject: [
+        { title: '전체', index: 1, query: '' },
+        { title: '수학', index: 2, query: '수학' },
+        { title: '영어', index: 3, query: '영어' },
+        { title: '국어', index: 4, query: '국어' },
+        { title: '과학', index: 5, query: '과학' },
+        { title: '사회', index: 6, query: '사회' },
+    ],
+};
